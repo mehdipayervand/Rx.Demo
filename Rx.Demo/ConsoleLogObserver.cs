90104881d2a6584c748550aad5ce045e66e232b3
Mehdi Payervand <mehdipayervand@outlook.com>
2023-04-04 08:45:57 +0330

    seperate classes

---
+++ Rx.Demo/ConsoleLogObserver.cs
@@ -0,0 +1,19 @@
+﻿namespace Rx.Demo;
+
+public class ConsoleLogObserver : IObserver<int>
+{
+    public void OnCompleted()
+    {
+        Console.WriteLine("Completed");
+    }
+
+    public void OnError(Exception error)
+    {
+        Console.WriteLine($"Error: {error}");
+    }
+
+    public void OnNext(int value)
+    {
+        Console.WriteLine($"Even Number is:{value}");
+    }
+}
---
+++ Rx.Demo/EvenNumberSubject.cs
@@ -0,0 +1,34 @@
+﻿using System.Reactive.Subjects;
+
+namespace Rx.Demo;
+
+class EvenNumberSubject : IDisposable
+{
+    // for cash event we should use ReplySubject
+    // for print the last one we can use BehaviorSubject
+    // for using SyncSubject we sould call complete method exac after subject.OnNext then console will print the last one item
+    private readonly Subject<int> subject = new ();
+    private readonly List<IDisposable> disposables = new();
+
+    public void Dispose()
+    {
+        subject?.Dispose();
+        foreach (var disposable in disposables)
+        {
+            disposable.Dispose();
+        }
+    }
+
+    public void Run()
+    {
+        Enumerable.Range(1, 100).Where(x => x % 2 == 0).ToList().ForEach(x =>
+        {
+            subject.OnNext(x);
+        });
+    }
+
+    public void Subscribe(Action<int> action)
+    {
+        disposables.Add(subject.Subscribe(action));
+    }
+}
