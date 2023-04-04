86d94d84079545da24a430a2fdcfc3b0e8d8055e
Mehdi Payervand <mehdipayervand@outlook.com>
2023-04-04 08:44:37 +0330

    Add project files.

---
+++ Rx.Demo/NumberObservable.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rx.Demo
+{
+    public class EvenNumberObservable : IObservable<int>
+    {
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            Enumerable.Range(1, 100).Where(x => x % 2 == 0).ToList().ForEach(x =>
+            {
+                observer.OnNext(x);
+            });
+
+            return Disposable.Empty;
+        }
+    }
+
+    public class ConsoleLogObserver : IObserver<int>
+    {
+        public void OnCompleted()
+        {
+            Console.WriteLine("Completed");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine($"Error: {error}");
+        }
+
+        public void OnNext(int value)
+        {
+            Console.WriteLine($"Even Number is:{value}");
+        }
+    }
+
+    class EvenNumberSubject : IDisposable
+    {
+        // for cash event we should use ReplySubject
+        // for print the last one we can use BehaviorSubject
+        // for using SyncSubject we sould call complete method exac after subject.OnNext then console will print the last one item
+        private readonly Subject<int> subject = new ();
+        private readonly List<IDisposable> disposables = new();
+
+        public void Dispose()
+        {
+            subject?.Dispose();
+            foreach (var disposable in disposables)
+            {
+                disposable.Dispose();
+            }
+        }
+
+        public void Run()
+        {
+            Enumerable.Range(1, 100).Where(x => x % 2 == 0).ToList().ForEach(x =>
+            {
+                subject.OnNext(x);
+            });
+        }
+
+        public void Subscribe(Action<int> action)
+        {
+            disposables.Add(subject.Subscribe(action));
+        }
+    }
+}
+ 
---
+++ Rx.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reactive.Linq;
+
+namespace Rx.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // 1:
+            //var evenNumber = new EvenNumberObservable();
+            //var consoleObserver = new ConsoleLogObserver();
+            //evenNumber.Subscribe(consoleObserver);
+
+            // 2:
+            //var evenNumber = new EvenNumberSubject();
+            //evenNumber.Subscribe(Console.WriteLine);
+            //evenNumber.Run();
+
+            Observable.Range(1,100)
+                .Where(c=>c%2==0)
+                //.Take(10)
+                //.Count()
+                //.Max()
+                // ...
+                //.Merge(Another Observable!)
+                .Subscribe(Console.WriteLine);
+
+
+
+            Console.WriteLine("Completed");
+            Console.ReadKey();
+        }
+    }
+}
